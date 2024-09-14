f19b31ab6bf20e76311bf6f6beaddc5f11365e15
Juan Vieira <juanvieira1@yahoo.com>
2024-09-14 11:54:14 -0300

    Tracking Model: model is ready for production on MLflow

--- src/models/train.py
+++ src/models/train.py
@@ -61,6 +61,7 @@ def main():
         'seed': args.seed
     }
 
+    mlflow.set_tracking_uri('http://127.0.0.1:5000')
     mlflow.set_experiment('house-prices-script-py')
 
     with mlflow.start_run(run_name='XGboost'):
