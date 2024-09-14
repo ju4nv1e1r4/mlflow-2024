9d4bf9715017ccdbc3a77314cc16dfdd9913d03d
Juan Vieira <juanvieira1@yahoo.com>
2024-09-13 21:29:15 -0300

    Ref: Commit refs to streamlit interface

--- main.py
+++ main.py
@@ -8,23 +8,23 @@ def predict(tamanho, ano, garagem):
     logged_model = 'runs:/c2cb855ab8be4a8693cfc6975f739b35/model'
     loaded_model = mlflow.pyfunc.load_model(logged_model)
 
-    df = {
-        'columns': ['tamanho, ano, garagem'],
-        'data': [[tamanho, ano, garagem]]   
-        }
+    data = {
+        'tamanho': [tamanho],
+        'ano': [ano],
+        'garagem': [garagem]
+    }
 
+    df = pd.DataFrame(data)
     predicted = loaded_model.predict(df)
 
-    data = pd.DataFrame(predicted)
-    
-    data.to_csv('reports/precos_preditos.csv', index=False)
+    return (predicted[0])
 
-    return data
+# print(predict(150, 2003, 2))
 
-
-tamanho = st.number_input('Qual o tamanho da casa?')
-ano = st.number_input('Qual o ano da casa?')
-garagem = st.number_input('Quantas garagens tem a casa?') 
+tamanho = float(st.number_input('Qual o tamanho da casa?'))
+ano = int(st.number_input('Qual o ano da casa?'))
+garagem = int(st.number_input('Quantas garagens tem a casa?') )
 
 if st.button('Prever'):
-    st.markdown(':blue[**O preço da casa é:**]', predict(tamanho, ano, garagem))    
+    pred = predict(tamanho, ano, garagem)
+    st.markdown(':blue[**O preço da casa é:**]', pred)
